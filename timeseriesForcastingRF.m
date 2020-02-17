6135be9b38445f1c72c4ad8c73634c77790e2098
evaenglert <55494887+evaenglert@users.noreply.github.com>
2020-02-17 15:42:20 +0000

    Update timeseriesForcastingRF.m
    added comments

--- timeseriesForcastingRF.m
+++ timeseriesForcastingRF.m
@@ -5,6 +5,7 @@ function X =  timeseriesForcasting(X)
 X.ForecastData.Price = X.RawData.Price(X.RawData.Date>=datenum(X.Config.Forecasting.Analysis.StartDate) & X.RawData.Date <= datenum(X.Config.Forecasting.Analysis.EndDate),:);
 X.ForecastData.Date = X.RawData.Date(X.RawData.Date>=datenum(X.Config.Forecasting.Analysis.StartDate) & X.RawData.Date <= datenum(X.Config.Forecasting.Analysis.EndDate));
 
+%results for different stocks will be saved here
 X.Results.Arma = cell(1,length(X.Config.Forecasting.Analysis.Assets));
 X.Results.RF = cell(1,length(X.Config.Forecasting.Analysis.Assets));
 X.Results.Hybrid = cell(1,length(X.Config.Forecasting.Analysis.Assets));
@@ -27,10 +28,6 @@ for i=1:length(X.Config.Forecasting.Analysis.Assets)
     TS =  armaTSForcastingRF(TS,X.Config.Forecasting.Model.ARMA);
 
     % save the result of the causality analysis to the X struct
-    %X.ForecastData.Arma.ForecastedValue = TS.ForecastedValue;
-    %X.ForecastData.Arma.OriginalValue = TS.OriginalValue;
-    %X.ForecastData.Arma.Date = TS.Date;
-    
     X.Results.Arma{i} = TS.ForecastedValue;
     X.Results.Original{i} = TS.OriginalValue;
 
@@ -39,37 +36,24 @@ for i=1:length(X.Config.Forecasting.Analysis.Assets)
     TS.Value = Price;
     TS.Date = Date;
     TS =  RandomForestTSForcasting(TS,X.Config.Forecasting.Model.RF);
-
-    %save the result of the casuality analysis to the X struct
-    %X.ForecastData.RF.ForecastedValue = TS.ForecastedValue;
-    %X.ForecastData.RF.OriginalValue = TS.OriginalValue;
-    %X.ForecastData.RF.Date = TS.Date;
     
+    % save the result of the causality analysis to the X struct
     X.Results.RF{i} = TS.ForecastedValue;
 
     % forecast with hybrid model
     TS.Value = Price;
     TS.Date = Date;
     TS =  hybridTSForcasting(TS,X.Config.Forecasting.Model.ARMA);
-
-    %save the result of the casuality analysis to the X struct
-    %X.ForecastData.Hybrid.ForecastedValue = TS.ForecastedValue;
-    %X.ForecastData.Hybrid.OriginalValue = TS.OriginalValue;
-    %X.ForecastData.Hybrid.Date = TS.Date;
     
+    % save the result of the causality analysis to the X struct
     X.Results.Hybrid{i} = TS.ForecastedValue;
 
     % forecast with ensemble model
     TS.Value = Price;
     TS.Date = Date;
-    %TS =  ensembleTSForcasting(TS,X.Config.Forecasting.Model);
-
-    %save the result of the casuality analysis to the X struct
-    %X.ForecastData.Ensemble.ForecastedValue = (X.ForecastData.RF.ForecastedValue + X.ForecastData.Arma.ForecastedValue)./2;
-    %X.ForecastData.Ensemble.OriginalValue = TS.OriginalValue;
-    X.ForecastData.Ensemble.Date = TS.Date;  
-    
+    X.ForecastData.Ensemble.Date = TS.Date;     
     X.Results.Ensemble{i} = (X.Results.RF{i} + X.Results.Arma{i})./2;
+    
     
     X.Results.Date = Date;
     
